58967875f940d920da93eaf62afae6a120512434
Liam Williams <liam.williams@zoho.com>
2016-06-04 14:56:27 +0100

    WeakSet implementation using caffeine

--- zdd/src/main/java/io/github/theangrydev/sandbox/zdd/WeakSet.java
+++ zdd/src/main/java/io/github/theangrydev/sandbox/zdd/WeakSet.java
@@ -1,37 +1,37 @@
 package io.github.theangrydev.sandbox.zdd;
 
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class WeakSet<Key> {
 
     private final ReferenceQueue<Key> referenceQueue = new ReferenceQueue<>();
-    private final ConcurrentHashMap<WeakKey<Key>, WeakKey<Key>> map = new ConcurrentHashMap<>();
+    private LoadingCache<WeakKey<Key>, WeakKey<Key>> cache;
+
+    public WeakSet() {
+        cache = Caffeine.newBuilder().build(key -> key);
+    }
 
     public Key putIfAbsent(Key key) {
-        purgeStaleKeys();
-        WeakKey<Key> weakKey = new WeakKey<>(key, referenceQueue);
-        WeakKey<Key> previous = map.putIfAbsent(weakKey, weakKey);
-        if (previous == null) {
-            return key;
+        purgeStaleKeys(); //TODO: could do this periodically
+        WeakKey<Key> unique = cache.get(new WeakKey<>(key, referenceQueue));
+        Key uniqueKey = unique.get();
+        if (uniqueKey != null) {
+            return uniqueKey;
         }
-        Key previousKey = previous.get();
-        if (previousKey != null) {
-            return previousKey;
-        }
-        // the key must have been garbage collected since the putIfAbsent
-        // this is a VERY unlucky case and so we just retry
         return putIfAbsent(key);
     }
 
     private void purgeStaleKeys() {
         Reference<? extends Key> reference;
         while ((reference = referenceQueue.poll()) != null) {
-            map.remove(reference);
+            cache.invalidate(reference);
         }
     }
 
