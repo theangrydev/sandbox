f2d5f9528024c30313e879e95cffeb4b6e8a021a
Liam Williams <liam.williams@zoho.com>
2016-06-05 20:31:00 +0100

    Moving union cache into zdd base

--- zdd/src/main/java/io/github/theangrydev/sandbox/zdd/ZDDBase.java
+++ zdd/src/main/java/io/github/theangrydev/sandbox/zdd/ZDDBase.java
@@ -1,5 +1,7 @@
 package io.github.theangrydev.sandbox.zdd;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.collect.Interner;
 import com.google.common.collect.Interners;
 
@@ -12,6 +14,27 @@ import static io.github.theangrydev.sandbox.zdd.ZeroZDD.ZERO_ZDD;
 public class ZDDBase {
 
     private final Interner<ZDD> zdds = Interners.newWeakInterner();
+    private final LoadingCache<ZDDBase.ZDDPair, ZDD> unions;
+
+    public ZDDBase() {
+        unions = Caffeine.newBuilder()
+                .softValues()
+                .build(zddPair -> zddPair.left.union(zddPair.right));
+    }
+
+    public ZDD union(ZDD left, ZDD right) {
+        return unions.get(new ZDDPair(left, right));
+    }
+
+    private static class ZDDPair extends ValueType {
+        private final ZDD left;
+        private final ZDD right;
+
+        private ZDDPair(ZDD left, ZDD right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
 
     public ZDD createZDD(ZDDVariable variable, ZDD thenZdd, ZDD elseZdd) {
         if (thenZdd == ZERO_ZDD) {
--- zdd/src/test/java/io/github/theangrydev/sandbox/zdd/ZDDBaseTest.java
+++ zdd/src/test/java/io/github/theangrydev/sandbox/zdd/ZDDBaseTest.java
@@ -3,8 +3,14 @@ package io.github.theangrydev.sandbox.zdd;
 import org.assertj.core.api.WithAssertions;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 import static io.github.theangrydev.sandbox.zdd.OneZDD.ONE_ZDD;
 import static io.github.theangrydev.sandbox.zdd.ZeroZDD.ZERO_ZDD;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 public class ZDDBaseTest implements WithAssertions {
 
@@ -49,5 +55,117 @@ public class ZDDBaseTest implements WithAssertions {
         ZDD laterOn = zddBase.createZDD(variable, thenZDD, elseZDD);
 
         assertThat(System.identityHashCode(laterOn)).isNotEqualTo(originalHash);
+    }
+
+
+    @Test
+    public void unionIsCached() {
+        ZDDBase zddBase = new ZDDBase();
+        ZDD left = mock(ZDD.class);
+        ZDD right = mock(ZDD.class);
+        ZDD union = mock(ZDD.class);
+        when(left.union(right)).thenReturn(union);
+
+        ZDD firstUnion = zddBase.union(left, right);
+        ZDD secondUnion = zddBase.union(left, right);
+
+        assertThat(firstUnion).isSameAs(union);
+        assertThat(secondUnion).isSameAs(union);
+
+        verify(left).union(right);
+        verifyNoMoreInteractions(left, right);
+    }
+
+    @Test
+    public void cacheIsClearedWhenSoftReferencesAreFreed() throws InterruptedException {
+        ZDDBase zddBase = new ZDDBase();
+
+        ZDDWithFixedUnion left = new ZDDWithFixedUnion();
+        ZDD right = mock(ZDD.class);
+
+        zddBase.union(left, right);
+        zddBase.union(left, right);
+        assertThat(left.unionCount).isEqualTo(1);
+
+        left.union = new RegularZDD(null, ZDDVariable.newVariable(1), OneZDD.ONE_ZDD, OneZDD.ONE_ZDD);;
+
+        forceSoftReferencesToBeCleared();
+
+        zddBase.union(left, right);
+        zddBase.union(left, right);
+        assertThat(left.unionCount).isEqualTo(2);
+    }
+
+    private void forceSoftReferencesToBeCleared() {
+        try {
+            List<long[]> memoryHog = new LinkedList<>();
+            while (true) {
+                memoryHog.add(new long[102400]);
+            }
+        } catch (OutOfMemoryError outOfMemoryError) {
+            System.out.println("Out of memory error produced; the GC must have cleared all the soft references by now!");
+        }
+    }
+
+    private static class ZDDWithFixedUnion implements ZDD {
+
+        private RegularZDD union = new RegularZDD(null, ZDDVariable.newVariable(1), OneZDD.ONE_ZDD, OneZDD.ONE_ZDD);
+        private int unionCount = 0;
+
+        @Override
+        public ZDDVariable variable() {
+            return null;
+        }
+
+        @Override
+        public ZDD thenZDD() {
+            return null;
+        }
+
+        @Override
+        public ZDD elseZDD() {
+            return null;
+        }
+
+        @Override
+        public ZDD union(ZDD zdd) {
+            unionCount++;
+            return union;
+        }
+
+        @Override
+        public ZDD intersection(ZDD zdd) {
+            return null;
+        }
+
+        @Override
+        public ZDD extend(ZDD zdd) {
+            return null;
+        }
+
+        @Override
+        public ZDD retainOverlapping(ZDD zdd) {
+            return null;
+        }
+
+        @Override
+        public ZDD removeAllElementsIn(ZDD zdd) {
+            return null;
+        }
+
+        @Override
+        public boolean contains(ZDD zdd) {
+            return false;
+        }
+
+        @Override
+        public Optional<ZDDVariable> directAssignment() {
+            return null;
+        }
+
+        @Override
+        public void appendSets(StringBuilder prefix, StringBuilder stringBuilder) {
+
+        }
     }
 }
